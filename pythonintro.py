c758e5373237340cc8346588e941bb1af267cc5d
Anniina <aspaju@utu.fi>
2018-11-03 14:44:47 +0200

    My Django Girls app, first commit

---
+++ pythonintro.py
@@ -0,0 +1,8 @@
+print("hello djangogirls")
+if 3>2:
+    print("it works")
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Anniina']
+for name in girls:
+    print(name)
+for i in range(1,6):
+    print(i)
